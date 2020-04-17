4dd0c160dfa7c8503e159e0ea8707b0476457c7a
shaozhun <712104878@qq.com>
2020-04-17 13:43:22 +0800

    添加产品

--- common/lang/en.js
+++ common/lang/en.js
@@ -8,5 +8,11 @@ export const lang = {
   slide2h1: 'Business efficiency requires a smooth flow of information',
   slide2p: 'As China`s demographic dividend is fading, today belongs to IT-powered new retail and smart manufacturing',
   slide3h1: 'AI, far more than a concept',
-  slide3p: 'No one can afford to be a bystander as the era of AI unveils.'
+  slide3p: 'No one can afford to be a bystander as the era of AI unveils.',
+  prod1h1: 'Enterprise Internet Portal Service',
+  prod1p: 'Integrating content manager for multilingual and multi-terminal usage.',
+  prod2h1: 'Enterprise Operation Support System',
+  prod2p: '5G and cloud computing will revolutionize how business operates.',
+  prod3h1: 'AI-integrated Development',
+  prod3p: 'Let the AI technology from Google, Amazon and Baidu pave your road to success.'
 }
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,16 +2,18 @@
   <div>
     <Home-Nav></Home-Nav>
     <Home-Swiper></Home-Swiper>
+    <Home-Product></Home-Product>
   </div>
 </template>
 
 <script>
 import HomeNav from './components/Nav'
 import HomeSwiper from './components/Swiper'
+import HomeProduct from './components/Product'
 export default {
   name: 'Home',
   components: {
-    HomeNav, HomeSwiper
+    HomeNav, HomeSwiper, HomeProduct
   },
   mounted () {
     // this.$i18n.locale = 'zh-CN'
