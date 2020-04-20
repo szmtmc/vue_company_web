855f0fd2fcb67441d1ea05ad1048f2100dacef3d
shaozhun <712104878@qq.com>
2020-04-20 13:54:55 +0800

    添加icon

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -65,6 +65,7 @@ const webpackConfig = merge(baseWebpackConfig, {
       filename: config.build.index,
       template: 'index.html',
       inject: true,
+      favicon: './favicon.ico',
       minify: {
         removeComments: true,
         collapseWhitespace: true,
